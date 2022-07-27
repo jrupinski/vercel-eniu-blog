9de36c79a7fe1b4f69ab86bbdbdb0d35cb24c1f1
jrupinski <rupinskijakub@gmail.com>
2022-07-28 00:31:02 +0200

    Rename Rupcio text

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am Rupcio Dupcio, welcome to my site.</p>
+        <p>I am Rupcio, welcome to my site.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
